f7af67b67a5172034744a412e0a7566abdd7bcf6
cbauzon <cbauzon@outlook.com>
2024-12-28 23:57:36 -0800

    Added get_status() and made read_reg() return a pointer

--- main/main.c
+++ main/main.c
@@ -17,25 +17,34 @@ void print_back(uint8_t addr, uint8_t data, rw read_or_write) {
     }
 }
 
-void read_reg(i2c_master_dev_handle_t dev_handle, uint8_t subaddr, uint8_t num_reads) {
+uint8_t * read_reg(i2c_master_dev_handle_t dev_handle, uint8_t subaddr, uint8_t num_reads, uint8_t alloc) {
     esp_err_t check;
 
-    uint8_t data_rd[num_reads];
-    check = i2c_master_transmit_receive(dev_handle, &subaddr, sizeof(uint8_t), &data_rd, num_reads*sizeof(uint8_t), -1);
+    uint8_t *data_rd = (uint8_t *) malloc(num_reads*sizeof(uint8_t));
+    check = i2c_master_transmit_receive(dev_handle, &subaddr, sizeof(uint8_t), data_rd, num_reads*sizeof(uint8_t), -1);
 
     if (check == ESP_OK) {
         for (uint8_t i = 0; i<num_reads; ++i) {
             print_back(subaddr, data_rd[i], READ);
         }
+        // printf("read_reg return: %p\n", data_rd);
+
+        if (alloc) return data_rd;
+        else {
+            free(data_rd);
+            return NULL;
+        }
     } else {
         printf("Failed to read from %02x!", subaddr);
     }
+    free(data_rd);
+    return NULL;
     
 }
 
 void who_am_i(i2c_master_dev_handle_t dev_handle) {
     uint8_t who_am_i_reg = 0x0f; 
-    read_reg(dev_handle, who_am_i_reg, 1);
+    read_reg(dev_handle, who_am_i_reg, 1, 0);
 }
 
 void write_reg(i2c_master_dev_handle_t i2c_dev, uint8_t subaddr, uint8_t write_val) {
@@ -82,6 +91,14 @@ void write_reg_multiple(i2c_master_dev_handle_t i2c_dev, uint8_t subaddr, uint8_
         
 }
 
+void get_status(i2c_master_dev_handle_t dev_handle) {
+    uint8_t *data_rd = read_reg(dev_handle, 0x27, 1, 1);
+    if (data_rd != NULL) printf("status data addr: %p, %02x\n", data_rd, *data_rd);
+    else printf("Error reading from 0x27!");
+    free(data_rd);
+
+}
+
 void app_main(void)
 {
     i2c_master_bus_handle_t i2c_mstr_handle;
@@ -93,7 +110,10 @@ void app_main(void)
     who_am_i(dev_handle);
     uint8_t write_vals[2] = {0x83, 0x81};
     write_reg_multiple(dev_handle, 0x20, write_vals, 2);
-    write_reg(dev_handle, 0x20, 0x80);
-    read_reg(dev_handle, 0x20, 1);
+    write_reg(dev_handle, 0x20, 0x83);
+    read_reg(dev_handle, 0x20, 2, 0);
+    read_reg(dev_handle, 0x21, 1, 0);
+    read_reg(dev_handle, 0x22, 1, 0);
+    get_status(dev_handle);
     printf("Done!\n");
 }
